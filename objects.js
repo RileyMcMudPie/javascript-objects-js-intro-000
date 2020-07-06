7dd0820f69c752a02dcbafda935096f6dc1cd365
Riley McMudPie <rydonamusic@gmail.com>
2020-07-06 19:37:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artist : 'song'}
 
 function updatePlaylist (playlist, artistName, songTitle){
-return Object.assign({artist : 'song'}, playlist, {[artistName] : songTitle})
+return Object.assign({artist : 'song'}, playlist, {artistName : songTitle})
 }
